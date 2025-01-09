537b3756c1b73409e92af710fc9063ffad05e8d5
Abed.Matini <abed.matini@ogilvy.co.za>
2025-01-09 12:10:34 +0200

    settings icons updated

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from "expo-router";
 import React from "react";
 import { Platform } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
@@ -20,7 +21,6 @@ export default function TabLayout() {
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: "absolute",
           },
           default: {},
@@ -41,7 +41,7 @@ export default function TabLayout() {
         options={{
           title: "Settings",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
+            <Ionicons name="settings-outline" size={28} color={color} />
           ),
         }}
       />
